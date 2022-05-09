f620ab3e5f46e6677ba4994a4e57182a966ccd4e
Shaik Noorullah Shareef <shaiknooru247@gmail.com>
2022-05-10 03:30:32 +0530

    website description

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "588049bad2e17c32"; }
+/******/ 		__webpack_require__.h = function() { return "de1921c358a7e3d3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -15,11 +15,11 @@ class MyDocument extends Document {
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta
             name="keywords"
-            content="HTML5 Template Avo onepage themeforest"
+            content="Creative Agency that focuses on creating mind blowing digital experiences"
           />
           <meta
             name="description"
-            content="Avo - Onepage Multi-Purpose HTML5 Template"
+            content="Creative Agency that focuses on creating mind blowing digital experiences"
           />
           <meta name="author" content="" />
           <link rel="shortcut icon" href="/img/favicon.ico" />
