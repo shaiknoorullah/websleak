1b4aca7bcaade964a198df32fa2670d533a154ff
Shaik Noorullah Shareef <shaiknooru247@gmail.com>
2022-10-23 04:51:36 +0530

    minor issues fixed

--- src/components/Project-details2-header/index.jsx
+++ src/components/Project-details2-header/index.jsx
@@ -1,63 +1,74 @@
-import React from "react";
-import Link from "next/link";
+/** @format */
+
+import React from "react"
+import Link from "next/link"
 
 const ProjectDetails2Header = () => {
-  return (
-    <section
-      className="page-header proj-det bg-img parallaxie valign"
-      style={{
-        backgroundImage:
-          "url(/img/profeceintNow/proficientwide200.webp 200w, proficientwide674.webp 674w, proficientwide986.webp 986w, proficientwide1205.webp 1205w, proficientwide1400.webp 1400w)",
-      }}
-      data-overlay-dark="4"
-    >
-      <div className="container">
-        <div className="row  ">
-          <div className="col-lg-7 col-md-9">
-            <div className="cont headerWrap ">
-              <h6>art &amp; illustration</h6>
-              <h2>Inspiring new space.</h2>
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-lg-3">
-            <div className="item mt-30">
-              <h6>Client</h6>
-              <p>
-                <a href="#0">Envato.com</a>
-              </p>
-            </div>
-          </div>
-          <div className="col-lg-3">
-            <div className="item mt-30">
-              <h6>Date</h6>
-              <p>6 August 2022</p>
-            </div>
-          </div>
-          <div className="col-lg-3">
-            <div className="item mt-30">
-              <h6>Categories</h6>
-              <p>
-                <Link href="/works/works-dark">Web Design </Link> ,
-                <Link href="/works/works-dark">WordPress</Link>
-              </p>
-            </div>
-          </div>
-          <div className="col-lg-3">
-            <div className="item mt-30">
-              <h6>Tags</h6>
-              <p>
-                <Link href="/works/works-dark">Minimal</Link> ,
-                <Link href="/works/works-dark">Modern</Link> ,
-                <Link href="/works/works-dark">Design</Link>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-};
+	return (
+		<section
+			className="page-header proj-det bg-img parallaxie valign"
+			style={{
+				backgroundImage:
+					"url(/img/profeceintNow/proficientwide200.webp 200w, proficientwide674.webp 674w, proficientwide986.webp 986w, proficientwide1205.webp 1205w, proficientwide1400.webp 1400w)",
+			}}
+			data-overlay-dark="4"
+		>
+			<div className="container">
+				<div className="row  ">
+					<div className="col-lg-7 col-md-9">
+						<div className="cont headerWrap ">
+							<h6>Brand Design, UI/UX & Development</h6>
+							<h2>ProficientNow</h2>
+						</div>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-lg-3">
+						<div className="item mt-30">
+							<h6>Client</h6>
+							<p>
+								<a href="#0">Envato.com</a>
+							</p>
+						</div>
+					</div>
+					<div className="col-lg-3">
+						<div className="item mt-30">
+							<h6>Date</h6>
+							<p>3rd October 2022</p>
+						</div>
+					</div>
+					<div className="col-lg-3">
+						<div className="item mt-30">
+							<h6>Categories</h6>
+							<p>
+								{/* <Link href="/works/works-dark">
+									Web Design{" "}
+								</Link>{" "} */}
+								Design & development.
+								{/* <Link href="/works/works-dark">
+                    WordPress
+                  </Link> */}
+							</p>
+						</div>
+					</div>
+					<div className="col-lg-3">
+						<div className="item mt-30">
+							<h6>Tags</h6>
+							<p>
+								{/* <Link href="/works/works-dark">
+									Minimal
+								</Link>{" "} */}
+								Brand Design, UI/UX Design, Front-end
+								development, & Back-end development.
+								{/* <Link href="/works/works-dark">Modern</Link>{" "} */}
+								{/* <Link href="/works/works-dark">Design</Link> */}
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
+}
 
-export default ProjectDetails2Header;
+export default ProjectDetails2Header
--- src/components/Project-details2-introduction/index.jsx
+++ src/components/Project-details2-introduction/index.jsx
@@ -1,37 +1,39 @@
-import React from "react";
+/** @format */
+
+import React from "react"
 
 const ProjectDetails2Introduction = () => {
-  return (
-    <section className="intro-section section-padding fontInMD">
-      <div className="container">
-        <div className="row  ">
-          <div className="col-lg-3 col-md-4">
-            <div className="htit">
-              <h4>
-                <span> 01 </span> Introduction{" "}
-              </h4>{" "}
-            </div>{" "}
-          </div>{" "}
-          <div className="col-lg-8 offset-lg-1 col-md-8">
-            <div className="text js-scroll__content">
-              <p className="extra-text">
-                We are a Creative Agency & amp; Startup Studio that provides
-                Digital Products and Services turns to focus on client
-                success.We specialize in user interface design, including front
-                - end development which we consider to be an integral part.{" "}
-              </p>
-              <ul className="smp-list mt-30">
-                <li> Aenean sollicitudin </li>{" "}
-                <li> lorem quis bibendum auctor </li>{" "}
-                <li> nisi elit conseq uat ipsum </li>{" "}
-                <li> nec sagittis sem nibh id elit </li>{" "}
-              </ul>{" "}
-            </div>{" "}
-          </div>{" "}
-        </div>{" "}
-      </div>{" "}
-    </section>
-  );
-};
+	return (
+		<section className="intro-section section-padding fontInMD">
+			<div className="container">
+				<div className="row  ">
+					<div className="col-lg-3 col-md-4">
+						<div className="htit">
+							<h4>
+								<span> 01 </span> Introduction
+							</h4>
+						</div>
+					</div>
+					<div className="col-lg-8 offset-lg-1 col-md-8">
+						<div className="text js-scroll__content">
+							<p className="extra-text">
+								ProficientNow is the nextGen staffing
+								company with a simple yet meaningful mission
+								to create a connection between deserving
+								talent & deserving positions.
+							</p>
+							<ul className="smp-list mt-30">
+								<li> Brand Design </li>
+								<li> Front-end Development </li>
+								<li> Backend Development </li>
+								<li> Reusable API </li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
+}
 
-export default ProjectDetails2Introduction;
+export default ProjectDetails2Introduction
